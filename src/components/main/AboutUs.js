2ccb008feabf369325c0c7e012ab707db54f2b52
awadbilal <awadbilal99@gmail.com>
2021-09-20 14:04:49 +0300

    fixed photo problems

--- src/components/main/AboutUs.js
+++ src/components/main/AboutUs.js
@@ -16,7 +16,7 @@ function AboutUs() {
           <Row className="justify-content-md-around">
             <Col xs={12} md={5} className="gridSystem">
               <Card>
-                <Card.Img variant="top" src="https://media-exp1.licdn.com/dms/image/C4D03AQFlfZk5QUtgvw/profile-displayphoto-shrink_400_400/0/1617895953522?e=1631750400&v=beta&t=n7AAoOGlaBAaDjdD9L-D6r4CKdT8MC7bifnhLQ3J1Rc" />
+                <Card.Img variant="top" src="https://avatars.githubusercontent.com/u/81809505?v=4" />
                 <Card.Header style={{ fontSize: '1.5rem', textAlign: 'center'}}><b><i>Bilal Avvad</i></b></Card.Header>
                 <Card.Body style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center', alignItems: 'center'}}>
                   <a href='https://www.linkedin.com/in/bilal-avvad/'>LinkedIn</a><br />
@@ -27,7 +27,7 @@ function AboutUs() {
             </Col>
             <Col xs={12} md={5} className="gridSystem">
             <Card>
-                <Card.Img variant="top" src="https://media-exp1.licdn.com/dms/image/C4D03AQHELUnn3e3lrA/profile-displayphoto-shrink_400_400/0/1613770195795?e=1631750400&v=beta&t=bkvbJK2PPv3dyDSlj8Y0-BILWrWUm96QxOFgsnO5XrI" />
+                <Card.Img variant="top" src="https://avatars.githubusercontent.com/u/69003977?v=4" />
                 <Card.Header style={{ fontSize: '1.5rem', textAlign: 'center'}}><b><i>Khadija Hawa</i></b></Card.Header>
                 <Card.Body style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center', alignItems: 'center'}}>
                   <a href='https://www.linkedin.com/in/khadija-hawa-160158165/'>LinkedIn</a><br />
